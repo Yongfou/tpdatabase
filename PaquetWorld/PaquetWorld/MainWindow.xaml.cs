62d6dd9049104d51fd0925856ced45e6b5ab705d
Sébastien Paquet <s-paquet@hotmail.fr>
2017-09-22 16:48:53 -0400

    ObjetMonde

--- PaquetWorld/PaquetWorld/MainWindow.xaml.cs
+++ PaquetWorld/PaquetWorld/MainWindow.xaml.cs
@@ -29,19 +29,52 @@ namespace PaquetWorld
 
         private void btnOk_Click(object sender, RoutedEventArgs e)
         {
-            TestMonde();
+            //TestMonde();
+            TestObjetMonde();
         }
 
         #region Méthodes
 
+        /// <summary>
+        /// Auteur: Sébastien Paquet
+        /// Description: Méthode qui teste les méthode de la classe monde
+        /// Date:22-09-2017
+        /// </summary>
         public void TestMonde()
         {
-            //Monde.CreerMonde("premier", 1000, 1000);
-            //Monde.SupprimerMonde(76);
-            //Monde.SupprimerMonde(90);
-            //Monde.ModifierMonde(77, "Patate", 50, 50);
-            //Monde.ModifierMonde(90, "patate", 50, 50);
-            Monde.AfficherListeMonde();
+            using(Entities context = new Entities())
+            {
+                Monde.CreerMonde("premier", 1000, 1000);
+                Monde md = context.Mondes.First();
+                Monde.SupprimerMonde(md.Id);
+                Monde.SupprimerMonde(50);
+                Monde md2 = context.Mondes.First();
+                Monde.ModifierMonde(md2.Id, "Patate", 50, 50);
+                Monde.ModifierMonde(50, "patate", 50, 50);
+                Monde.AfficherListeMonde();
+            }
+           
+        }
+
+        /// <summary>
+        /// Auteur: Sébastien Paquet
+        /// Description:Méthode qui teste les méthodes de la classe objet monde
+        /// Date:22-09-2017
+        /// </summary>
+        public void TestObjetMonde()
+        {
+            using(Entities context = new Entities())
+            {
+                Monde md = context.Mondes.First();
+                ObjetMonde.CreerObjetMonde("Petite fleur", 50, 50, 3, md.Id);
+                ObjetMonde.CreerObjetMonde("Petite fleur", 50, 50, 3, 50);
+                ObjetMonde obm = context.ObjetMondes.First();
+                ObjetMonde.SupprimerObjetMonde(obm.Id);
+                ObjetMonde.SupprimerObjetMonde(50);
+                ObjetMonde obm2 = context.ObjetMondes.First();
+                ObjetMonde.ModifierObjetMonde("Poil", obm2.Id);
+                ObjetMonde.ModifierObjetMonde("Poil", 50);
+            }
         }
 
         #endregion
